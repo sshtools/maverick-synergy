71181a0ddf7601d1638ab5b24e97dad35939b248
Lee Painter <lee@javassh.com>
2019-08-17 10:32:23 +0100

    License header check and format.

--- maverick-virtual-session/src/main/java/com/sshtools/server/vsession/PosixChannelPtyTerminal.java
+++ maverick-virtual-session/src/main/java/com/sshtools/server/vsession/PosixChannelPtyTerminal.java
@@ -1,10 +1,20 @@
-/*
- * Copyright (c) 2002-2018, the original author or authors.
+/**
+ * (c) 2002-2019 JADAPTIVE Limited. All Rights Reserved.
  *
- * This software is distributable under the BSD license. See the terms of the
- * BSD license in the documentation provided with this software.
+ * This file is part of the Maverick Synergy Java SSH API.
  *
- * https://opensource.org/licenses/BSD-3-Clause
+ * Maverick Synergy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Maverick Synergy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.sshtools.server.vsession;
 

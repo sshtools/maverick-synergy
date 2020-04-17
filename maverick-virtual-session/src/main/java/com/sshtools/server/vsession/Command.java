a8a5682b9e9aef1078c6f8ea21759d32cc31ce9a
Lee Painter <lee@ludup.com>
2020-04-17 13:30:33 +0100

    Added license headers to new files.

--- maverick-virtual-session/src/main/java/com/sshtools/server/vsession/Command.java
+++ maverick-virtual-session/src/main/java/com/sshtools/server/vsession/Command.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.jline.reader.Candidate;
-import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.ParsedLine;
 

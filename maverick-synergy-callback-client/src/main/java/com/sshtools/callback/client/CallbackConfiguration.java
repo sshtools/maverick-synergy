897d3d469565256b6617f9167172f222e5065b32
Lee Painter <lee@javassh.com>
2021-10-14 20:03:42 +0100

    HOTFIX: Missing assignment of memo.

--- maverick-synergy-callback-client/src/main/java/com/sshtools/callback/client/CallbackConfiguration.java
+++ maverick-synergy-callback-client/src/main/java/com/sshtools/callback/client/CallbackConfiguration.java
@@ -53,6 +53,7 @@ public class CallbackConfiguration {
 		this.serverPort = serverPort;
 		this.privateKey = privateKey;
 		this.publicKey = publicKey;
+		this.memo = memo;
 	}
 	
 	protected CallbackConfiguration() {

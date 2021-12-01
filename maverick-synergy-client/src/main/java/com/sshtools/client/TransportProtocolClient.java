9201e1cedf156d8babca9592d33c1d7669235fe4
Lee Painter <lee@javassh.com>
2021-12-01 19:26:46 +0000

    HOTFIX: Removed all warnings.

--- maverick-synergy-client/src/main/java/com/sshtools/client/TransportProtocolClient.java
+++ maverick-synergy-client/src/main/java/com/sshtools/client/TransportProtocolClient.java
@@ -22,7 +22,6 @@ package com.sshtools.client;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 
 import com.sshtools.common.events.Event;

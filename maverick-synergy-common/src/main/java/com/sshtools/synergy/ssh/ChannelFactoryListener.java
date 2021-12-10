d4891a84e1d3ea26f33461dbfe001c17279513ec
Lee Painter <lee@javassh.com>
2021-12-10 21:57:08 +0000

    HOTFIX: Added a ChannelFactoryListener to DefaultServerChannelFactory. Enforce each channel declares getChannelType method by adding it to the Channel interface.

--- maverick-synergy-common/src/main/java/com/sshtools/synergy/ssh/ChannelFactoryListener.java
+++ maverick-synergy-common/src/main/java/com/sshtools/synergy/ssh/ChannelFactoryListener.java
@@ -18,41 +18,9 @@
  *
  * https://www.jadaptive.com/app/manpage/en/article/1565029/What-third-party-dependencies-does-the-Maverick-Synergy-API-have
  */
+package com.sshtools.synergy.ssh;
 
-package com.sshtools.common.ssh;
+public interface ChannelFactoryListener<T extends SshContext> {
 
-import java.io.IOException;
-
-public interface Channel {
-
-	int getLocalWindow();
-
-	int getRemoteWindow();
-
-	int getLocalPacket();
-
-	void close();
-
-	void sendData(byte[] array, int i, int size) throws IOException;
-
-	void sendWindowAdjust(int bytesSinceLastWindowIssue);
-
-	boolean isClosed();
-
-	void addEventListener(ChannelEventListener listener);
-
-	void sendChannelRequest(String requestName, boolean wantReply, byte[] data);
-
-	void sendChannelRequest(String type, boolean wantreply,
-			byte[] requestdata, ChannelRequestFuture future);
-
-	SshConnection getConnection();
-
-	Context getContext();
-
-	boolean isRemoteEOF();
-
-	boolean isLocalEOF();
-
-	String getChannelType();
+	default void onChannelCreated(ChannelNG<T> channel) {  }
 }

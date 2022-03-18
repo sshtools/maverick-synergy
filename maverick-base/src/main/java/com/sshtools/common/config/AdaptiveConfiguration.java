56e6e19d85489c64dd6e1d04794a3b7eda1fb433
ludup <lee@javassh.com>
2022-03-18 21:46:13 +0000

    Added license headers

--- maverick-base/src/main/java/com/sshtools/common/config/AdaptiveConfiguration.java
+++ maverick-base/src/main/java/com/sshtools/common/config/AdaptiveConfiguration.java
@@ -1,14 +1,20 @@
-/*
- * Copyright (c) 2003-2021 JADAPTIVE Limited. All Rights Reserved.
+/**
+ * (c) 2002-2021 JADAPTIVE Limited. All Rights Reserved.
  *
- * This file contains Original Code and/or Modifications of Original Code and
- * its use is subject to the terms of the JADAPTIVE Software License. You may not use
- * this file except in compliance with the license terms.
+ * This file is part of the Maverick Synergy Java SSH API.
  *
- * https://www.jadaptive.com/en/content/api-software-license
+ * Maverick Synergy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Email: support@jadaptive.com
- * WWW: https://www.jadaptive.com
+ * Maverick Synergy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Maverick Synergy.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.sshtools.common.config;
 

beaccf6e5383ca10ab5836f1a13f5b94cec259bd
Lee Painter <lee@javassh.com>
2022-08-10 18:24:50 +0100

    HOTFIX: Allow passing of shell command to VirtualFileFactory.

--- maverick-virtual-session/src/main/java/com/sshtools/server/vsession/VirtualChannelFactory.java
+++ maverick-virtual-session/src/main/java/com/sshtools/server/vsession/VirtualChannelFactory.java
@@ -31,16 +31,22 @@ import com.sshtools.synergy.ssh.ChannelNG;
 public class VirtualChannelFactory extends DefaultServerChannelFactory {
 
 	CommandFactory<? extends ShellCommand>[] factories;
-	
+	String shellCommand;
 	@SafeVarargs
 	public VirtualChannelFactory(CommandFactory<? extends ShellCommand>... factories) {
 		this.factories = factories;
 	}
-	 
+
+	@SafeVarargs
+	public VirtualChannelFactory(String shellCommand, CommandFactory<? extends ShellCommand>... factories) {
+		this.factories = factories;
+		this.shellCommand = shellCommand;
+	}
+	
 	@Override
 	protected ChannelNG<SshServerContext> createSessionChannel(SshConnection con)
 			throws UnsupportedChannelException, PermissionDeniedException {
-		return new VirtualShellNG(con,  new ShellCommandFactory(factories));
+		return new VirtualShellNG(con,  new ShellCommandFactory(factories), shellCommand);
 	}
 	
 	protected CommandFactory<? extends ShellCommand>[] getCommandFactories() {

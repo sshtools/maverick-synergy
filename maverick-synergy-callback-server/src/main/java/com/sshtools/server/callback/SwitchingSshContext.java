f8167187ba59ebbd2b40062deae6becc83d795a7
Lee Painter <lee@javassh.com>
2022-10-13 16:42:09 +0100

    Allow security level to be passed to SwitchingSshContext

--- maverick-synergy-callback-server/src/main/java/com/sshtools/server/callback/SwitchingSshContext.java
+++ maverick-synergy-callback-server/src/main/java/com/sshtools/server/callback/SwitchingSshContext.java
@@ -24,6 +24,7 @@ package com.sshtools.server.callback;
 import java.io.IOException;
 
 import com.sshtools.client.SshClientContext;
+import com.sshtools.common.ssh.SecurityLevel;
 import com.sshtools.common.ssh.SshException;
 import com.sshtools.server.SshServerContext;
 import com.sshtools.synergy.nio.ConnectRequestFuture;
@@ -40,6 +41,16 @@ public class SwitchingSshContext extends SshClientContext {
 			String clientIdentifier,
 			ProtocolContextFactory<SshServerContext> serverFactory) throws IOException, SshException {
 		super(engine);
+		this.clientIdentifier = clientIdentifier;
+		this.serverFactory = serverFactory;
+		setSoftwareVersionComments("CallbackServer");
+	}
+	
+	public SwitchingSshContext(SshEngine engine, 
+			String clientIdentifier,
+			ProtocolContextFactory<SshServerContext> serverFactory,
+			SecurityLevel securityLevel) throws IOException, SshException {
+		super(engine, securityLevel);
 		this.clientIdentifier = clientIdentifier;
 		this.serverFactory = serverFactory;
 		setSoftwareVersionComments("CallbackServer");

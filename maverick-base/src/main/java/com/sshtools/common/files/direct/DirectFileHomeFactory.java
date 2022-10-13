c04c8ad79ba531010b2f1af5a8f2e13de7649443
Lee Painter <lee@javassh.com>
2022-10-13 16:39:40 +0100

    Support Windows 10/11 home folders as well as previous Windows versions.

--- maverick-base/src/main/java/com/sshtools/common/files/direct/DirectFileHomeFactory.java
+++ maverick-base/src/main/java/com/sshtools/common/files/direct/DirectFileHomeFactory.java
@@ -28,6 +28,8 @@ public class DirectFileHomeFactory {
 		String os = System.getProperty("os.name");
 		if(os.startsWith("Mac OS X"))
 			return "/Users/" + con.getUsername();
+		else if(os.startsWith("Windows 1"))
+			return "/Users/" + con.getUsername();
 		else if(os.startsWith("Windows"))
 			return "/Documents and Settings/" + con.getUsername();
 		else

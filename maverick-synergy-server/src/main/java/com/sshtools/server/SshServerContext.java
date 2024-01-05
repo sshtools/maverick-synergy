b7d727adf2328fb753886cfb165eccd8501a342d
Lee Painter <lee@javassh.com>
2024-01-05 21:41:22 +0000

    Organised imports

--- maverick-synergy-server/src/main/java/com/sshtools/server/SshServerContext.java
+++ maverick-synergy-server/src/main/java/com/sshtools/server/SshServerContext.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 import java.util.ServiceLoader;

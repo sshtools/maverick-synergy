bfbfc2b91c9cc49ba8f4a6bcf95993276b33d62c
ludup <lee@logonbox.com>
2024-01-24 13:09:15 +0000

    Read single byte from AbstractRandomAccess

--- maverick-base/src/main/java/com/sshtools/common/files/AbstractFileRandomAccess.java
+++ maverick-base/src/main/java/com/sshtools/common/files/AbstractFileRandomAccess.java
@@ -1,12 +1,13 @@
 package com.sshtools.common.files;
 
+import java.io.Closeable;
 import java.io.IOException;
 
-public interface AbstractFileRandomAccess {
+public interface AbstractFileRandomAccess extends Closeable {
 	public int read(byte[] buf, int off, int len) throws IOException;
 	public void write(byte[] buf, int off, int len) throws IOException;
 	public void setLength(long length) throws IOException;
 	public void seek(long position) throws IOException;
-	public void close() throws IOException;
 	public long getFilePointer() throws IOException;
+	public int read() throws IOException;
 }

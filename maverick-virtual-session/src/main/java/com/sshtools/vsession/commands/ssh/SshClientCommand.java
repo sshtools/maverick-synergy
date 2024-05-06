22d5815538e66304dbffef2db914473ad7274d1b
Lee Painter <lee@ludup.com>
2024-05-06 13:45:15 +0100

    Attempting simple change that does not use "raw" mode.

--- maverick-virtual-session/src/main/java/com/sshtools/vsession/commands/ssh/SshClientCommand.java
+++ maverick-virtual-session/src/main/java/com/sshtools/vsession/commands/ssh/SshClientCommand.java
@@ -33,7 +33,7 @@ public class SshClientCommand extends AbstractSshClientCommand {
 	@Override
 	public void runCommand(SshClient sshClient, SshClientArguments arguments, VirtualConsole console) {
 
-		console.getSessionChannel().enableRawMode();
+		//console.getSessionChannel().enableRawMode();
 		
 		try {
 			Connection<SshClientContext> connection = sshClient.getConnection();		
@@ -84,7 +84,7 @@ public class SshClientCommand extends AbstractSshClientCommand {
 			task.waitForever();
 
 		} finally {
-			console.getSessionChannel().disableRawMode();
+			//console.getSessionChannel().disableRawMode();
 			console.println();
 		}
 

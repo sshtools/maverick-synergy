b44186eeecb78f389819339feb31e1f9bc1a218b
Lee Painter <lee@javassh.com>
2025-08-21 17:20:06 +0100

    Make constructor public to see if withSession will work

--- maverick-synergy-client/src/main/java/com/sshtools/client/tasks/AbstractSessionTask.java
+++ maverick-synergy-client/src/main/java/com/sshtools/client/tasks/AbstractSessionTask.java
@@ -44,7 +44,7 @@ public abstract class AbstractSessionTask<T extends AbstractSessionChannel> exte
 	/**
 	 * Builder for {@link AbstractSessionTask}.
 	 */
-	protected static abstract class AbstractSessionTaskBuilder<
+	public static abstract class AbstractSessionTaskBuilder<
 			B extends AbstractSessionTaskBuilder<B, SC, TT>,
 			SC extends AbstractSessionChannel,
 			TT extends AbstractSessionTask<SC>> extends AbstractConnectionTaskBuilder<B, TT> { 

ad22801769c00392d3849aaf67be7a08fe2746bf
Brett Smith <brett@logonbox.com>
2026-02-27 11:14:47 +0000

    Improved file logger to be more resilient to external changes to log file.

--- maverick-logging/src/main/java/com/sshtools/common/logger/FileLoggingContext.java
+++ maverick-logging/src/main/java/com/sshtools/common/logger/FileLoggingContext.java
@@ -28,6 +28,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import com.sshtools.common.logger.Log.Level;
 import com.sshtools.common.util.IOUtils;
@@ -39,6 +41,10 @@ public class FileLoggingContext extends AbstractLoggingContext {
 	BufferedWriter currentWriter = null;
 	OutputStream currentOut = null;
 	RandomAccessFile currentFile = null;
+	Object currentFileKey = null;
+	long lastWriteAt = 0L;
+	long suppressedUntil = 0L;
+	long idleCloseMillis;
 	
 	long maxSize;
 	int maxFiles;
@@ -46,10 +52,15 @@ public class FileLoggingContext extends AbstractLoggingContext {
 	boolean logging = true;
 
 	public FileLoggingContext(Level level, File logFile) throws IOException {
-		this(level, logFile, 10, 1024 * 1024 * 20L);
+		this(level, logFile, 10, 1024 * 1024 * 20L, 30000L);
 	}
 	
 	public FileLoggingContext(Level level, File logFile, int maxFiles, long maxSize) throws IOException {
+		this(level, logFile, maxFiles, maxSize, 30000L);
+	}
+	
+	public FileLoggingContext(Level level, File logFile, int maxFiles, long maxSize, long idleCloseMillis)
+			throws IOException {
 		super(level);
 		this.logFile = logFile;
 		if(!logFile.exists()) {
@@ -57,6 +68,7 @@ public class FileLoggingContext extends AbstractLoggingContext {
 		}
 		this.maxFiles = maxFiles;
 		this.maxSize = maxSize;
+		this.idleCloseMillis = idleCloseMillis;
 		createLogFile();
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
@@ -69,13 +81,23 @@ public class FileLoggingContext extends AbstractLoggingContext {
 		currentFile = new RandomAccessFile(logFile, "rw");
 		currentFile.seek(currentFile.length());
 		currentWriter = new BufferedWriter(new OutputStreamWriter(new RandomAccessOutputStream(currentFile)), 65536);
+		currentFileKey = readFileKey();
 		log(Level.INFO, String.format("Logging file %s", logFile.getAbsolutePath()), null);
+	}
+
+	private Object readFileKey() {
+		try {
+			BasicFileAttributes attrs = Files.readAttributes(logFile.toPath(), BasicFileAttributes.class);
+			return attrs.fileKey();
+		} catch (IOException e) {
+			return null;
+		}
 	}
 
 
 	@Override
 	public boolean isLogging(Level level) {
-		return logging && super.isLogging(level);
+		return super.isLogging(level) && (logging || System.currentTimeMillis() >= suppressedUntil);
 	}
 	
 	@Override
@@ -84,34 +106,106 @@ public class FileLoggingContext extends AbstractLoggingContext {
 	}
 
 	private synchronized void logToFile(String msg, boolean flush) {
+		long now = System.currentTimeMillis();
+		if(!logging && now < suppressedUntil) {
+			return;
+		}
 		try {
+			maybeCloseIfIdle(now);
+			ensureOpen();
 			checkRollingLog();
 			if(currentFile.getChannel().isOpen()) {
 				currentWriter.write(msg);
 				if(flush) {
 					currentWriter.flush();
 				}
+				lastWriteAt = now;
+				logging = true;
 			}
 		} catch (IOException e) {
 			System.err.println(String.format("Failed to log to %s", logFile.getName()));
 			e.printStackTrace();
+			suppressedUntil = now + 5000L;
 			logging = false;
+			tryRecover(msg, flush);
+		}
+	}
+
+	private void tryRecover(String msg, boolean flush) {
+		try {
+			closeLog();
+			ensureOpen();
+			if(currentFile != null && currentFile.getChannel().isOpen()) {
+				currentWriter.write(msg);
+				if(flush) {
+					currentWriter.flush();
+				}
+				lastWriteAt = System.currentTimeMillis();
+				logging = true;
+			}
+		} catch (IOException ignored) {
+			// Suppress repeated failures; next attempt will retry after backoff.
+		}
+	}
+
+	private void maybeCloseIfIdle(long now) {
+		if(idleCloseMillis > 0 && lastWriteAt > 0 && (now - lastWriteAt) >= idleCloseMillis) {
+			closeLog();
+		}
+	}
+
+	private void ensureOpen() throws IOException {
+		if(currentFile == null || currentWriter == null || !currentFile.getChannel().isOpen()) {
+			createLogFile();
+			logging = true;
 		}
 	}
 
 	private void closeLog() {
 		IOUtils.closeStream(currentWriter);
 		IOUtils.closeStream(currentOut);
+		IOUtils.closeStream(currentFile);
+		currentWriter = null;
+		currentOut = null;
+		currentFile = null;
+		currentFileKey = null;
 	}
 	
 	private synchronized void checkRollingLog() throws IOException {
 
-		if(currentFile.getChannel().isOpen()) {
-			if(currentFile.length() > maxSize) {
+		if(currentFile == null || !currentFile.getChannel().isOpen()) {
+			return;
+		}
+
+		if(!logFile.exists()) {
+			closeLog();
+			createLogFile();
+			return;
+		}
+
+		BasicFileAttributes attrs = Files.readAttributes(logFile.toPath(), BasicFileAttributes.class);
+		Object fileKey = attrs.fileKey();
+		if(fileKey != null && currentFileKey != null && !fileKey.equals(currentFileKey)) {
+			closeLog();
+			createLogFile();
+			return;
+		}
+
+		long currentLength = currentFile.length();
+		long pathLength = attrs.size();
+		if(pathLength < currentLength) {
+			if(fileKey == null || currentFileKey == null) {
 				closeLog();
-				IOUtils.rollover(logFile, maxFiles);
 				createLogFile();
+				return;
 			}
+			currentFile.seek(pathLength);
+		}
+
+		if(currentLength > maxSize) {
+			closeLog();
+			IOUtils.rollover(logFile, maxFiles);
+			createLogFile();
 		}
 	}
 
